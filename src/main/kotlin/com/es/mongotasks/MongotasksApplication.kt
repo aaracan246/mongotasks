93c60d58a0eabfd3ed5125133f167b72d7817120
Morri <acalberto@iesrafaelalberti.es>
2025-02-14 20:59:25 +0100

    Initial commit

---
+++ src/main/kotlin/com/es/mongotasks/MongotasksApplication.kt
@@ -0,0 +1,11 @@
+package com.es.mongotasks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongotasksApplication
+
+fun main(args: Array<String>) {
+	runApplication<MongotasksApplication>(*args)
+}
